69e260139c645d71de8f5f6923e4ec6eff135b39
Caio Duarte Santos <caio.duarte@dtidigital.com.br>
2021-06-04 22:47:37 -0300

    Correção

--- api/controllers/classificatorController.js
+++ api/controllers/classificatorController.js
@@ -2,12 +2,17 @@ const classificatorService = require("../services/classificatorService");
 
 exports.rateImage = async (req, res) => {
   const image = req.files["image"];
-
-  const classificationResult = await classificatorService.detectImageObjects(
-    image.tempFilePath
-  );
-  const x = classificationResult[0].class;
-  return res.json({
-    x,
-  });
+  try {
+    const classificationResult = await classificatorService.detectImageObjects(
+      image.tempFilePath
+    );
+    const x = classificationResult[0].class;
+    return res.json({
+      x,
+    });
+  } catch (error) {
+    return res.json({
+      error,
+    });
+  }
 };
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,5 +4,10 @@ const classficatorController = require("../controllers/classificatorController")
 const router = express.Router();
 
 router.post("/classificator", classficatorController.rateImage);
+router.get("/healthcheck", (req, res) => {
+  return res.json({
+    check: true,
+  });
+});
 
 module.exports = router;
